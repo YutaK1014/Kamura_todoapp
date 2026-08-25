39eeefaca15e905e3bf21f2b46a9f90fc3f6eab5
LAPTOP-Q8ISVSD0\academia <yuta20021014@gmail.com>
2026-08-25 12:50:44 +0900

    入力チェック追加

--- src/main/java/com/example/todoapp/HomeController.java
+++ src/main/java/com/example/todoapp/HomeController.java
@@ -2,6 +2,7 @@ package com.example.todoapp;
 
 import java.util.List;
 
+import org.springframework.validation.BindingResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class HomeController {
@@ -38,7 +41,10 @@ public class HomeController {
     }
 
     @PostMapping("/todos/confirm")
-    public String createConfirm(@ModelAttribute Todo todo, Model model) {
+    public String createConfirm(@Valid @ModelAttribute Todo todo, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "create";
+        }
         model.addAttribute("todo", todo);
         return "create-confirm";
     }
@@ -69,7 +75,12 @@ public class HomeController {
     }
 
     @PostMapping("/todos/{id}/confirm")
-    public String editConfirm(@PathVariable Long id, @ModelAttribute Todo todo, Model model) {
+    public String editConfirm(@PathVariable Long id, @Valid @ModelAttribute Todo todo,
+            BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("id", id);
+            return "edit";
+        }
         model.addAttribute("todo", todo);
         model.addAttribute("id", id);
         return "edit-confirm";
