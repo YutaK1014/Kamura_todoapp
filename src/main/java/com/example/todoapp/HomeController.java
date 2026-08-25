d9e1ca42aaddaa09322689b7b1971caa60783c50
LAPTOP-Q8ISVSD0\academia <yuta20021014@gmail.com>
2026-08-25 10:35:49 +0900

    一覧画面作成

--- src/main/java/com/example/todoapp/HomeController.java
+++ src/main/java/com/example/todoapp/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.todoapp;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +9,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    private final TodoMapper todoMapper;
+
+    public HomeController(TodoMapper todoMapper) {
+        this.todoMapper = todoMapper;
+    }
+
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("pageTitle", "やること管理");
         return "index";
+    }
+    @GetMapping("/todos")
+    public String todos(Model model) {
+        List<Todo> todos = todoMapper.findAll();
+        model.addAttribute("todos", todos);
+        return "todos";
     }
 }
